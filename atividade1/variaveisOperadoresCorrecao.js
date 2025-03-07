f43f5611b4e1a309de71615d2b3d45c527868b9a
MariaEduardaRibeiro0212 <mariaeduardaribeiro0212@gmail.com>
2025-03-07 08:55:03 -0300

    correcao da atividade1

---
+++ atividade1/variaveisOperadoresCorrecao.js
@@ -0,0 +1,20 @@
+let num_A = 2.47;
+let num_B = 3.14;
+let num_C;
+
+//Adição
+
+num_C = num_A + num_B;
+console.log(`O resultado da adição entre ${num_A} + ${num_B} é ${num_C}`);
+
+//Subtração
+num_C = num_A - num_B;
+console.log(`O resultado da subtração entre ${num_A} - ${num_B} é ${num_C}`);
+
+//Multiplicação
+num_C = num_A * num_B;
+console.log(`O resultado da multiplicação entre ${num_A} X ${num_B} é ${num_C}`);
+
+//Divisão
+num_C = num_A / num_B;
+console.log(`O resultado da adiçãoentre ${num_A} / ${num_B} é ${num_C}`);
